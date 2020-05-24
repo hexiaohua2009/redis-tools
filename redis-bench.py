689fe25a49e41b7244fae5e65eb7a63dbd0b67fd
hexh <hexiaohua2009@hotmail.com>
2020-05-24 20:49:05 +0800

    构建方法增加密码参数

--- redis-bench.py
+++ redis-bench.py
@@ -6,10 +6,11 @@
 from rediscluster import RedisCluster
 import sys
 
+# 封装redis cluster对象
 class redisClusterHelper():
-    def __init__(self,startup_nodes):
+    def __init__(self,startup_nodes,password):
         try:
-            self.redisconn = RedisCluster(startup_nodes=startup_nodes,password='123456',decode_responses=True)
+            self.redisconn = RedisCluster(startup_nodes=startup_nodes,password=password,decode_responses=True)
         except Exception as e:
             print("Connect Error: {0}".format(e))
             sys.exit()
@@ -25,9 +26,12 @@ if __name__ == '__main__':
         {'host': '192.168.0.34', 'port': 6379},
         {'host': '192.168.0.32', 'port': 6380}
     ]
-    rch = redisClusterHelper(startup_nodes)
+    # 创建实例
+    rch = redisClusterHelper(startup_nodes,'123456')
+    # 插入数据
     count = 0
-    while count < 1000000:
+    kvs = 1000000
+    while count < kvs:
         rch.set_key("name" + str(count), 'admin' + str(count))
         print(rch.get_key('name'+ str(count)))
         count = count + 1
